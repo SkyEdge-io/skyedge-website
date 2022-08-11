2bfd51324796ebb8b0f233bdb35965f7d5d51b02
Ramanjs <raman.s2662@gmail.com>
2022-08-12 00:33:49 +0530

    added social buttons

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,4 +1,7 @@
 import './About.css';
+import linkedin from '../../assets/linkedin.svg';
+import twitter from '../../assets/twitter.svg';
+import discord from '../../assets/discord.svg';
 
 const About = () => {
   return (
@@ -6,8 +9,14 @@ const About = () => {
       <h1>About</h1>
       <div className="about-cards-container">
         <div className="about-card">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin elit aliquam justo, urna, neque. Et viverra at facilisis dictum aliquet commodo. Aenean sed id feugiat cursus sit at ut volutpat. Tempor magnis cursus etiam nunc diam dignissim.</div>
+        
         <div className="about-card">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin elit aliquam justo, urna, neque. Et viverra at facilisis dictum aliquet commodo. Aenean sed id feugiat cursus sit at ut volutpat. Tempor magnis cursus etiam nunc diam dignissim.</div>
         <div className="about-card">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin elit aliquam justo, urna, neque. Et viverra at facilisis dictum aliquet commodo. Aenean sed id feugiat cursus sit at ut volutpat. Tempor magnis cursus etiam nunc diam dignissim.</div>
+        <div className="socials">
+          <span className="social-acc"><img src={linkedin} /></span>
+          <span className="social-acc"><img src={twitter} /></span>
+          <span className="social-acc"><img src={discord} /></span>
+        </div>
       </div>
     </div>
   )
