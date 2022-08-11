50ba67878226f47867d9aecb23dc7b2d1006a911
Ramanjs <raman.s2662@gmail.com>
2022-08-11 19:13:26 +0530

    media queries for mobile UI

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -6,6 +6,8 @@ const About = () => {
       <h1>About</h1>
       <div className="about-cards-container">
         <div className="about-card">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin elit aliquam justo, urna, neque. Et viverra at facilisis dictum aliquet commodo. Aenean sed id feugiat cursus sit at ut volutpat. Tempor magnis cursus etiam nunc diam dignissim.</div>
+        <div className="about-card">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin elit aliquam justo, urna, neque. Et viverra at facilisis dictum aliquet commodo. Aenean sed id feugiat cursus sit at ut volutpat. Tempor magnis cursus etiam nunc diam dignissim.</div>
+        <div className="about-card">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin elit aliquam justo, urna, neque. Et viverra at facilisis dictum aliquet commodo. Aenean sed id feugiat cursus sit at ut volutpat. Tempor magnis cursus etiam nunc diam dignissim.</div>
       </div>
     </div>
   )
--- src/components/Roadmap/Roadmap.js
+++ src/components/Roadmap/Roadmap.js
@@ -21,6 +21,25 @@ const Roadmap = () => {
             </div>
           </div>
         </div>
+        <div className="roadmap-steps-container">
+          <div className="roadmap-step">
+            <img src={Step1} className="roadmap-step-image" />
+            <div className="roadmap-step-hero">
+              <h2>Testnet</h2>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin elit aliquam justo, urna, neque. Et viverra at facilisis dictum aliquet commodo. Aenean sed id</p>
+            </div>
+          </div>
+        </div>
+         <div className="roadmap-steps-container">
+          <div className="roadmap-step">
+            <img src={Step1} className="roadmap-step-image" />
+            <div className="roadmap-step-hero">
+              <h2>Testnet</h2>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin elit aliquam justo, urna, neque. Et viverra at facilisis dictum aliquet commodo. Aenean sed id</p>
+            </div>
+          </div>
+        </div>
+ 
       </div>
     </div>
   )
