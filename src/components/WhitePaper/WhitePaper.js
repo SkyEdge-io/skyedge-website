50a3c38ae5cbd474fe0df794a3b28cb8c0cee4ac
Ramanjs <raman.s2662@gmail.com>
2022-08-27 16:45:11 +0530

    whitepaper slideshow

--- src/components/WhitePaper/WhitePaper.js
+++ src/components/WhitePaper/WhitePaper.js
@@ -1,23 +1,82 @@
 import './WhitePaper.css';
 import whitepaper from '../../assets/write-paper.svg';
+import {useState} from 'react';
+import {useEffect} from 'react';
+import step1 from '../../assets/steps/step1.svg';
+import step2 from '../../assets/steps/step2.svg';
+import step3 from '../../assets/steps/step3.svg';
+import step4 from '../../assets/steps/step4.svg';
 
 const WhitePaper = () => {
+
+  const stepsInitial = [
+    {
+      id: 1,
+      text: 'Step 1',
+      isActive: false,
+      url: step1,
+    },
+    {
+      id: 2,
+      text: 'Step 2',
+      isActive: false,
+      url: step2,
+    },
+    {
+      id: 3,
+      text: 'Step 3',
+      isActive: false,
+      url: step3,
+    },
+    {
+      id: 4,
+      text: 'Step 4',
+      isActive: false,
+      url: step4,
+    },
+  ];
+
+  const [index, setIndex] = useState(1);
+  const [steps, setSteps] = useState(JSON.parse(JSON.stringify(stepsInitial)));
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setIndex((index) => {
+        if (index + 1 > 4) return 1;
+        return index + 1;
+      });
+    }, 3000);
+
+    return () => clearInterval(intervalId);
+  }, []);
+
+  useEffect(() => {
+    const stepsCopy = JSON.parse(JSON.stringify(steps));
+    stepsCopy.forEach(step => {
+      if (step.id == index) step.isActive = true;
+      else step.isActive = false;
+    });
+    setSteps(stepsCopy);
+  }, [index]);
+
   return (
     <div className="container whitepaper">
       <h1>How we transform</h1>
       <div className="whitepaper-content">
-        <img src={whitepaper} alt="img" style={{width: "12vw"}}/>
+        <img src={whitepaper} alt="img" style={{width: "12vw"}} className="whitepaper-img"/>
         <div className="steps-container">
-          <span className="step">Step 1</span>
-          <span className="step">Step 2</span>
-          <span className="step">Step 3</span>
-          <span className="step">Step 4</span>
+          {steps.map(step => (
+            <div key={step.id} className={step.isActive ? "step active-step" : "step"} >{step.text}</div>
+          ))}
         </div>
-        <div className="whitepaper-hero">
-          <span className="whitepaper-hero-content">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin elit aliquam justo, urna, neque. Et viverra at facilisis dictum aliquet commodo. Aenean sed id
-          </span>
-        </div>
+        {steps.map(step => (
+          <div className={step.isActive ? "whitepaper-hero active-whitepaper-hero" : "whitepaper-hero"}>
+            <img src={step.url} className="whitepaper-hero-img"/>
+            <span className="whitepaper-hero-content">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin elit aliquam justo, urna, neque. Et viverra at facilisis dictum aliquet commodo. Aenean sed id
+            </span>
+          </div>
+        ))}
       </div>
     </div>
   )
