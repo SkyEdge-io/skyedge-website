95d716c2584967e5f73584f18282e6939aad0663
Ramanjs <raman.s2662@gmail.com>
2022-09-03 22:13:17 +0530

    updated tokenomics

--- src/components/Tokenomics/Tokenomics.js
+++ src/components/Tokenomics/Tokenomics.js
@@ -5,10 +5,33 @@ const Tokenomics = () => {
     <div className="container tokenomics">
       <h1>Tokenomics</h1>
       <div className="token-container">
-        <div className="token">
-          <div className="token-img"></div>
-          <h2 className="token-header">Token Details</h2>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed arcu, gravida vitae tristique viverra ut amet, amet sit. Eget facilisis proin facilisis interdum ac tincidunt egestas lobortis.</p>
+        <div className="token-col1">
+          <div className="token-card">
+            <h2>Private Token Sales - 1 <span>5.0%</span></h2>
+            <p>SkyEdge runs on mobile devices enabling Digital assets and privacy-first AI at scale. We are making distributed computing and blockchain native to billions of mobile devices.</p>
+          </div>
+          <div className="token-card">
+            <h2>Private Token Sales - 2 <span>7.0%</span></h2>
+            <p>SkyEdge runs on mobile devices enabling Digital assets and privacy-first AI at scale. We are making distributed computing and blockchain native to billions of mobile devices.</p>
+          </div>
+        </div>
+        <div className="token-col2">
+          <div className="token-col2-row col2-row1">
+            <div className="token-card">
+              <h2>Private Token Sales - 2 <span>7.0%</span></h2>
+              <p>SkyEdge runs on mobile devices enabling Digital assets and privacy-first AI at scale. We are making distributed computing and blockchain native to billions of mobile devices.</p>
+            </div>
+            <div className="token-card">
+              <h2>IDO - Public Offering <span>15.1%</span></h2>
+              <p>SkyEdge runs on mobile devices enabling Digital assets and privacy-first AI at scale. We are making distributed computing and blockchain native to billions of mobile devices.</p>
+            </div>
+          </div>
+          <div className="token-col2-row col2-row2">
+            <div className="token-card">
+              <h2>Pre - IDO Public Sales <span>2.5%</span></h2>
+              <p>SkyEdge runs on mobile devices enabling Digital assets and privacy-first AI at scale. We are making distributed computing and blockchain native to billions of mobile devices.</p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
